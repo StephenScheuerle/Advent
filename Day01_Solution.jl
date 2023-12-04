f5ff160419c0a1c77fbad629d04cf46c59287a02
Stephen Scheuerle <113196406+StephenScheuerle@users.noreply.github.com>
2023-12-04 17:01:34 -0500

    Day 1
    Added the solution and input document for day 1

---
+++ Day01_Solution.jl
@@ -0,0 +1,14 @@
+
+const filename = "C:\\Users\\Steph\\OneDrive\\Documents\\AdventofCode\\2023\\Day01.txt"
+
+
+function getCalibrationValue()
+    f = readlines(filename)
+    for i = 1:length(f)
+        g = split(f[i])    
+    end
+    println(g)
+end
+
+
+
